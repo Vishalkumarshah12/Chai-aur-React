dc28d48bb2213469e41f5fd4621bb11496105841
Vishalkumarshah12 <bishalshah23412@gmail.com>
2023-11-07 20:01:53 +0545

    react

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind Test</h1>
+      <h1 className="bg-green-400 text-white p-5 rounded-xl mb-4">Tailwind Test</h1>
+     <Card/>
     </>
-  )
+  ) 
 }
 
 export default App
